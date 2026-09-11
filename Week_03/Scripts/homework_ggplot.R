921d321676a134b0e26e7af3e24ec837ef1df05f
weishen23 <weishen@hawaii.edu>
2026-09-10 21:55:46 -1000

    Homework Update

--- Week_03/Scripts/homework_ggplot.R
+++ Week_03/Scripts/homework_ggplot.R
@@ -26,6 +26,7 @@ my_plot <- ggplot(data,
            color = species)) +
   geom_violin(fill = "white") + #violin plot
   geom_beeswarm() + #swarm plot
+  geom_boxplot(fill = NA) +
   facet_wrap(~ sex, ncol = 2) + #wrap by species, and make it two columns
   guides(color = "none") + #remove color legend
   labs(title = "Body mass of penguins in Palmer Archipelago, Antarctica",
